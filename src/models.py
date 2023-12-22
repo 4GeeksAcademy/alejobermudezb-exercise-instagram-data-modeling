d3a81492485ea85ae01ba321d83f53a67c450b2b
Luis Alejandro Bermudez <142993857+Alejobermudezb@users.noreply.github.com>
2023-12-22 09:17:13 +0000

    feat: primer commit con fallos mentales XD

--- src/models.py
+++ src/models.py
@@ -1,29 +1,70 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)  
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    phone = Column(Integer)
+    birthday = Column(String(10), nullable=False)
+    posts = relationship("Post", backref="user")
+    messages = relationship("Message")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    date = Column(String(250))
+    location = Column(String(250))
+    like = Column(String(250), nullable=False)
+    description = Column(String(250))
+    user = relationship("User")
+    pictures = relationship("Picture")
+    comments = relationship("Comment")
+
+class Update(Base):
+    __tablename__ = 'update'
+    id = Column(Integer, primary_key=True)
+    date = Column(String(250))
+    viewer = Column(String(250))
+    reaction = Column(String(250), nullable=False)
+    description = Column(String(250), ForeignKey('user.id'))
+    pictures = relationship("Picture", backref="update")
+    comments = relationship("Comment")
+
+
+class Message(Base):
+    __tablename__ = 'message'
+    id = Column(Integer, primary_key=True)
+    text = Column(Text, nullable=False)
+    reaction = Column(String(5))
+    date = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+   
+class Followers(Base):
+    __tablename__ = 'followers'
+    id = Column(Integer, primary_key=True)
+    followers_id = Column(String(250))
+    following_id = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    update_id = Column(Integer, ForeignKey('update.id'))
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    text = Column(Text, nullable=False)
+    like = Column(Boolean)
+    date = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    update_id = Column(Integer, ForeignKey('update.id'))
+
 
     def to_dict(self):
         return {}
